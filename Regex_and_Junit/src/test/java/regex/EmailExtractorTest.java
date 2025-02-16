9b4b9d810c93acefa6588076e368256f575cafde
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-02-16 19:44:34 +0530

    [Naincy Shrivastava] add Email Extractor Test

--- Regex_and_Junit/src/test/java/regex/EmailExtractorTest.java
+++ Regex_and_Junit/src/test/java/regex/EmailExtractorTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 
-class EmailExtractorTest {
+class EmailExtractorTest{
 
     @Test
     void testExtractEmails() {
