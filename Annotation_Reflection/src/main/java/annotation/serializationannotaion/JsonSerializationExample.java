1ef839f4e6a431b9d3d52820eddc1041b7fd5f4e
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-02-18 16:43:32 +0530

    [Naincy Shrivastava] add custom serialization annotation

---
+++ Annotation_Reflection/src/main/java/annotation/serializationannotaion/JsonSerializationExample.java
@@ -0,0 +1,14 @@
+package annotation.serializationannotaion;
+public class JsonSerializationExample {
+
+    public static void main(String[] args) {
+        User user = new User("Abhishek", 22, "Pass@123");
+
+        try {
+            String jsonString = JsonSerializer.toJson(user);
+            System.out.println("Generated JSON: " + jsonString);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Annotation_Reflection/src/test/java/JsonSerializationExampleTest.java
@@ -0,0 +1,20 @@
+
+
+import annotation.serializationannotaion.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsonSerializationExampleTest {
+    @Test
+    void testJsonSerialization() throws IllegalAccessException {
+        User user = new User("Abhi", 30, "password123");
+        String json = JsonSerializer.toJson(user);
+
+        assertTrue(json.contains("\"user_name\": \"Abhi\""));
+        assertTrue(json.contains("\"user_age\": \"30\""));
+        assertFalse(json.contains("password"));
+    }
+
+}
