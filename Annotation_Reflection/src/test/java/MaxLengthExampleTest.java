6637f91d52d39f8ee08e50e892359e00f1bea422
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-02-18 15:33:39 +0530

    [Naincy Shrivastava] add Creating @executionTimeannotation for field validation

---
+++ Annotation_Reflection/src/test/java/MaxLengthExampleTest.java
@@ -0,0 +1,24 @@
+
+
+
+import annotation.maxlengthannotation.User;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxLengthExampleTest {
+
+    @Test
+    void testValidUsername() {
+        User user = new User("Abhi");
+        assertEquals("Abhi", user.getUsername());
+    }
+
+    @Test
+    void testInvalidUsername() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new User("Abhishek123");
+        });
+
+        assertEquals("Username exceeds max length of 10 characters.", exception.getMessage());
+    }
+}
