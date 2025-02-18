06aa3f1a65fe983a166a776b0e112f9cbfba96fe
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-02-18 14:59:28 +0530

    [Naincy Shrivastava] add Marking important method

---
+++ Annotation_Reflection/src/test/java/MarkImportantMethodTest.java
@@ -0,0 +1,35 @@
+
+import annotation.markimportantmethod.ImportantMethod;
+
+import annotation.customannotation.TaskManager;
+import org.junit.jupiter.api.Test;
+import java.lang.reflect.Method;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class MarkImportantMethodTest {
+    @Test
+    void testImportantMethodsAnnotation() {
+        Class<?> cls = TaskManager.class;
+        Method[] methods = cls.getDeclaredMethods();
+
+        int importantMethodsCount = 0;
+        for (Method method : methods) { 
+            if (method.isAnnotationPresent(ImportantMethod.class)) {
+                ImportantMethod annotation = method.getAnnotation(ImportantMethod.class);
+                assertNotNull(annotation, "Annotation should not be null");
+                importantMethodsCount++;
+                System.out.println("Im under the water");
+
+                if (method.getName().equals("processCriticalTask")) {
+                    assertEquals("HIGH", annotation.level());
+                } else if (method.getName().equals("processRegularTask")) {
+                    assertEquals("MEDIUM", annotation.level());
+                }
+            }
+        }
+        assertEquals(2, importantMethodsCount);
+    }
+
+
+}
