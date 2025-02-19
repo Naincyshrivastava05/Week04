d35db7783cf38fe9391e1340756ea6bc403cbbe6
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-02-19 12:06:36 +0530

    [Naincy Shrivastava] add access and modify private field

---
+++ Annotation_Reflection/src/main/java/reflaction/intermediatelevel/accessandmodifyprivatefield/Configuration.java
@@ -0,0 +1,11 @@
+package reflaction.intermediatelevel.accessandmodifyprivatefield;
+
+
+
+public class Configuration {
+    private static String API_KEY = "9798345287";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
---
+++ Annotation_Reflection/src/main/java/reflaction/intermediatelevel/accessandmodifyprivatefield/ModifyStaticField.java
@@ -0,0 +1,20 @@
+package reflaction.intermediatelevel.accessandmodifyprivatefield;
+
+
+
+import java.lang.reflect.Field;
+
+public class ModifyStaticField {
+    public static void main(String[] args) throws Exception {
+        Class<?> clazz = Configuration.class;
+
+        Field field = clazz.getDeclaredField("API_KEY");
+        field.setAccessible(true);
+
+        System.out.println("Original API_KEY: " + Configuration.getApiKey());
+
+        field.set(null, "7079732862");
+
+        System.out.println("Modified API_KEY: " + Configuration.getApiKey());
+    }
+}
