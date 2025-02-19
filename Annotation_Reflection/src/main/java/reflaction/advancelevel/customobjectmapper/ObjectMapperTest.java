70d2fe7c2ad3768ec15d956d2ff6f398bfd38c1f
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-02-19 12:23:16 +0530

    [Naincy Shrivastava] add access and custom object mapper

---
+++ Annotation_Reflection/src/main/java/reflaction/advancelevel/customobjectmapper/ObjectMapperTest.java
@@ -0,0 +1,19 @@
+package reflaction.advancelevel.customobjectmapper;
+
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ObjectMapperTest {
+    public static void main(String[] args) {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("name", "Abhishek");
+        properties.put("age", 25);
+        properties.put("city", "Bhopal");
+
+        Person person = CustomObjectMapper.toObject(Person.class, properties);
+
+        person.displayDetails();
+    }
+}
---
+++ Annotation_Reflection/src/test/java/reflection/CustomObjectMapperTest.java
@@ -0,0 +1,41 @@
+package reflection;
+
+
+import org.junit.jupiter.api.Test;
+import reflaction.advancelevel.customobjectmapper.CustomObjectMapper;
+import reflaction.basiclevel.access_private_field.Person;
+
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CustomObjectMapperTest {
+    private Object getFieldValue(Object obj, String fieldName) {
+        try {
+            Field field = obj.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+            return field.get(obj);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Test
+    public void testToObject() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("name", "Naincy");
+        properties.put("age", 30);
+        properties.put("city", "Bhopal");
+
+        Person person = CustomObjectMapper.toObject(Person.class, properties);
+
+        assertNotNull(person);
+        assertEquals("Naincy", getFieldValue(person, "name"));
+        assertEquals(30, getFieldValue(person, "age"));
+        assertEquals("Bhopal", getFieldValue(person, "city"));
+    }
+
+
+}
